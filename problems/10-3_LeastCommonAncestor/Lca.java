8861a6db33a5fd99d3dd76b98219391128b75779
Ron Desmond <rhdsmnd@gmail.com>
2016-11-02 13:55:50 -0700

    memoize lca

--- problems/10-3_LeastCommonAncestor/Lca.java
+++ problems/10-3_LeastCommonAncestor/Lca.java
@@ -1,13 +1,18 @@
+import java.util.HashMap;
 
 /** Finds the "least common ancestor" (lca) of FIRSTBINTREE and SECONDBINTREE,
  *  both of which are in ROOT, where the lca is the node furthest from the root
  *  that contains both FIRSTBINTREE and SECONDBINTREE. */
-public class Lca {
-    public static<T> BinTree<T> findLca(BinTree<T> root, final BinTree<T> firstBinTree, final BinTree<T> secondBinTree) {
+public class Lca<T> {
+
+	public Lca() {
+		hasDescMap = new HashMap<BinTree<T>, HashMap<BinTree<T>, Boolean>>();
+	}
+
+    public BinTree<T> findLca(BinTree<T> root, final BinTree<T> firstBinTree, final BinTree<T> secondBinTree) {
         if (root == null || firstBinTree == null || secondBinTree == null) {
             return null;
         }
-
 		BinTree<T> leftChildRes = findLca((BinTree<T>) root.getChild(BinTree.LEFT), firstBinTree, secondBinTree);
 		if (leftChildRes != null) {
 			return leftChildRes;
@@ -25,14 +30,28 @@ public class Lca {
 		}
     }
 
-	public static<T> boolean hasDescendant(BinTree<T> root, final BinTree<T> descendant) {
-		// memoize
+	public boolean hasDescendant(BinTree<T> root, final BinTree<T> descendant) {
 		if (root == null) {
 			return false;
 		} else if (root.isEqual(descendant)) {
 			return true;
 		}
-		return hasDescendant((BinTree<T>) root.getChild(BinTree.LEFT), descendant) ||
-				hasDescendant((BinTree<T>) root.getChild(BinTree.RIGHT), descendant);
+
+		/** memoize: trade space for time */
+		if (!hasDescMap.containsKey(descendant)) {
+			hasDescMap.put(descendant, new HashMap<BinTree<T>, Boolean>());
+		}
+
+		HashMap<BinTree<T>, Boolean> containsDesc = hasDescMap.get(descendant);
+		if (containsDesc.containsKey(root)) {
+			return containsDesc.get(root);
+		}
+
+		containsDesc.put(root,
+				hasDescendant((BinTree<T>) root.getChild(BinTree.LEFT), descendant) ||
+				hasDescendant((BinTree<T>) root.getChild(BinTree.RIGHT), descendant));
+		return containsDesc.get(root);
 	}
+
+	private HashMap<BinTree<T>, HashMap<BinTree<T>, Boolean>> hasDescMap;
 }
